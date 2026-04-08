222d5503b95196e59191cb7efab3a640c1be8499
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-04-08 19:20:04 +0530

    Create union_packed_typedef.sv

---
+++ 4.UNION/union_packed/union_packed_typedef.sv
@@ -0,0 +1,40 @@
+module union_ex();
+  typedef union packed
+  {
+    bit[31:0]date;
+//  string month;
+    int year;
+   } dob;
+  dob dob_1;
+  dob dob_2;
+  
+  initial begin
+    
+    $display("---_original_----"); 
+    dob_1.date  =29;
+    $display("%0d",dob_1.date);
+    
+//  dob_1.month ="JAN";
+//  $display("%s",dob_1.month);
+    
+    dob_1.year  =2005;
+    $display("%0d",dob_1.year);
+    
+    $display("---_mod_----");
+    dob_2.date  =5'd17;
+    $display("%0d",dob_2.date);
+    
+    
+//  dob_2.month ="SEPT";
+//  $display("%s",dob_2.month);
+    
+    dob_2.year  =2004;
+    $display("%0d",dob_2.year);
+    
+   
+  end
+endmodule
+    
+    
+    
+    
