c23daefd321b9d15c47c458b123780490c5c1252
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-04-09 20:53:17 +0530

    Create forever_finish.sv

---
+++ 5.CONTROL_FLOW/LOOP/FOREVER/finish/forever_finish.sv
@@ -0,0 +1,42 @@
+// Design a watchdog using forever:
+
+// Start a counter
+// Reset counter when done == 1
+// If counter exceeds 50 cycles → print "Timeout Error" and $finish
+
+
+module control_flow();
+  
+  int count;
+  bit done;
+  initial begin
+   
+    $display("------------");
+    
+    forever begin
+    #5
+      if(done==1)begin
+        count=0;
+        $display("ITS_UR_TURN at %0t",$time);
+      end
+      else 
+        count++;
+      
+      if(count==50)begin
+        $display("TIME_ERROR at %0t",$time);
+        $finish;
+      end
+      
+    end
+    $display("------------");
+  end
+  
+  initial begin
+        done =0;
+    #60 done =1;
+    #5  done =0;
+  end
+    
+endmodule
+      
+      
